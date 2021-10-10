3c4ea31cf572f1370ffe95a07fb907b298c7cf30
Derek Olson <derek@derek-o.com>
2021-10-09 22:48:06 -0500

    add badboys tone and police lights animation

--- thotalyzer0xb.ino
+++ thotalyzer0xb.ino
@@ -1,4 +1,6 @@
 #include "TC_IS31FL3731.h"
+#include "touch.h"
+#include "buzzer.h"
 
 #define INPIN 26
 #define I2C_ADDR 0x74
@@ -7,6 +9,7 @@
 #define LED_SDB 22u
 
 TC_IS31FL3731 tcleds = TC_IS31FL3731();
+bool isActive = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,11 +25,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  uint16_t drankLevel = analogRead(INPIN);
   clearBar();
+  if(checkTouch(START_TOUCH, TOUCH_THRESH) == 1) {
+    isActive = !isActive;
+    delay(300);
+  }
+
+  if(!isActive) {
+    tcleds.setDisplayMode(Display_Mode_Picture);
+    return;
+  }
+
+  uint16_t drankLevel = analogRead(INPIN);
   if (drankLevel>2700) {
     setBar(0b1000000, 255, 0, 0);
+    playBadBoys();
   }
   if (drankLevel>2650) {
     setBar(0b0100000, 255, 108, 0);
@@ -58,4 +71,39 @@ void clearBar()
 {
   tcleds.setTextBotColor(0x7F, 0, 0, 0);
   tcleds.setTextTopColor(0x7F, 0, 0, 0);
+}
+
+void playBadBoys() {
+  flashBar(tcleds);
+  uint16_t len = sizeof(badBoys) / sizeof(Tone);
+  for (uint16_t i=0; i<len; i++)
+  {
+    tone(BUZZER_PIN, badBoys[i].note, badBoys[i].hold*60, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+  }
+}
+
+void flashBar(TC_IS31FL3731 ledController) {
+  ledController.setAutoPlayFrames(2);
+  ledController.setAutoPlayLoops(0);
+  ledController.setAutoPlayDelay(175);
+  ledController.setAutoPlayStart(1);
+
+  // these should already be 0'd if this is called on startup
+  ledController.setAllLEDPWM(0, 1);
+  ledController.setAllLEDPWM(0, 2);
+
+  // prepare the show
+  ledController.setTextTopColor(0b1110000, 255, 0, 0, 1);
+  ledController.setTextBotColor(0b1110000, 255, 0, 0, 1);
+  ledController.setTextTopColor(0b0000111, 0, 0, 0, 1);
+  ledController.setTextBotColor(0b0000111, 0, 0, 0, 1);
+
+  ledController.setTextTopColor(0b0000111, 0, 0, 255, 2);
+  ledController.setTextBotColor(0b0000111, 0, 0, 255, 2);
+  ledController.setTextTopColor(0b1110000, 0, 0, 0, 2);
+  ledController.setTextBotColor(0b1110000, 0, 0, 0, 2);
+
+  // showtime!
+  ledController.setDisplayMode(Display_Mode_Auto_Play);
 }
